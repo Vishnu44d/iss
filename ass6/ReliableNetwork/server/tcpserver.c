6a567ef6cfa3e6806bd6d70ff071f040d9470307
vishnu <vishnu44d@gmail.com>
2020-06-04 19:04:46 +0530

    files for server and clients

--- ass6/ReliableNetwork/server/tcpserver.c
+++ ass6/ReliableNetwork/server/tcpserver.c
@@ -8,12 +8,34 @@
 #include <arpa/inet.h>
 
 #define PORT 5555
+#define MAX_CONNECTION 5
+
+typedef struct node
+{
+    int socket_d;
+    struct sockaddr_in address;
+    socklen_t address_size;
+
+} node;
+
+node *init_q(int socket, struct sockaddr_in addr)
+{
+    node *Q = (node *)malloc(sizeof(node));
+    Q->socket_d = -1;
+    Q->address.sin_addr = addr.sin_addr;
+    Q->address.sin_family = addr.sin_family;
+    Q->address.sin_port = addr.sin_port;
+    return Q;
+}
 
 int main()
 {
 
     int sockfd, ret;
     struct sockaddr_in serverAddr;
+    int connCount = 1;
+
+    node *Q[25];
 
     int newSocket;
     struct sockaddr_in newAddr;
@@ -35,6 +57,13 @@ int main()
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // First elelment of queue is always server
+    Q[0] = init_q(sockfd, serverAddr);
+    FILE *fp;
+    fp = fopen("connections.csv", "w+");
+    fprintf(fp, "%d,%s,%d\n", sockfd, inet_ntoa(serverAddr.sin_addr), ntohs(serverAddr.sin_port));
+    fclose(fp);
 
     ret = bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
     if (ret < 0)
@@ -60,6 +89,10 @@ int main()
         {
             exit(1);
         }
+        // Q[connCount++] = init_q(newSocket, newAddr);
+        fp = fopen("connections.csv", "a");
+        fprintf(fp, "%d,%s,%d\n", newSocket, inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+        fclose(fp);
         printf("[SERVER]: Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
 
         if ((childpid = fork()) == 0)
@@ -70,6 +103,7 @@ int main()
             {
                 memset(&buffer, '\0', sizeof(buffer));
                 recv(newSocket, buffer, 1024, 0);
+
                 if (strcmp(buffer, ":exit") == 0)
                 {
                     printf("[SERVER]: Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
---
+++ ass6/ReliableNetwork/utils.h
@@ -0,0 +1,1 @@
+// for writing the helper function to send and recieve files between sockets.
